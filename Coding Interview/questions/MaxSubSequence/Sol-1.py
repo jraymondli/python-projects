f472ecadd5ef0fdaa920f0d2fde2f7c9848c6679
JIAN LI <raymond.li.jian@gmail.com>
2024-10-20 18:41:26 -0700

    Update Sol-1.py

--- Coding Interview/questions/MaxSubSequence/Sol-1.py
+++ Coding Interview/questions/MaxSubSequence/Sol-1.py
@@ -1,3 +1,7 @@
+# Observations: we can get max of the array to be a starting point and use two pointers i, j
+# 1. meaningful starting point, i, woulb be a positive number
+# 2. at the point the partial sum is <= 0, we need to move i to j 
+
 def max_sub_array(nums):
   n = len(nums)
   i, j = 0, 0
