6271ee12f13d13e6dc239aa17e7dc2c428e8a4d7
JIAN LI <raymond.li.jian@gmail.com>
2024-10-26 09:15:39 -0700

    Create RaySolution.py

---
+++ Coding Interview/questions/ProductArrayExceptSelf/LongestIncreasingSubsequence/RaySolution.py
@@ -0,0 +1,12 @@
+def longest_subsequence(nums):
+    
+    if not nums: return 0
+    n = len(nums)
+    
+    dp = [1] * n 
+    for i in range(1, n):
+      for j in range(0, i):
+        if nums[j] < nums[i]:
+          dp[i] = max(dp[i], dp[j] +1)
+          
+    return max(dp)
