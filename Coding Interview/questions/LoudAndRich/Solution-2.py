7dbe442a88d54a1b6a4f8436814dd8ad9f6c5163
JIAN LI <raymond.li.jian@gmail.com>
2024-10-25 19:34:25 -0700

    Update Solution-2.py

--- Coding Interview/questions/LoudAndRich/Solution-2.py
+++ Coding Interview/questions/LoudAndRich/Solution-2.py
@@ -7,20 +7,20 @@ def loud_and_rich(richer, quiet):
     
     adj = defaultdict(lambda:[])
     adjr = defaultdict(lambda:[])
-    d_resolved = {}
     for a, b in richer:
       adj[b].append(a)
       adjr[a].append(b)
-    dq = deque()
+        
+    dq, d_resolved = deque(), {}
     for i in range(n):
       d_resolved[i] = len(adj[i])
       if d_resolved[i] == 0:
         dq.append(i)
+          
     while dq:
       i = dq.popleft()
       for nxt in adj[i]:
-        if quiet[res[nxt]] < quiet[res[i]]:
-          res[i] = res[nxt]
+        if quiet[res[nxt]] < quiet[res[i]]: res[i] = res[nxt]
       for nxt in adjr[i]:
         d_resolved[nxt] -= 1
         if d_resolved[nxt] == 0: dq.append(nxt)
