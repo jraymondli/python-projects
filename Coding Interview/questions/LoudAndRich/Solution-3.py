30e612d709df27361e693570fe352a6a1ee0549b
JIAN LI <raymond.li.jian@gmail.com>
2024-10-25 19:40:07 -0700

    Create Solution-3.py

---
+++ Coding Interview/questions/LoudAndRich/Solution-3.py
@@ -0,0 +1,27 @@
+from collections import defaultdict 
+from collections import deque
+
+def loud_and_rich(richer, quiet):
+    n = len(quiet)
+    res = list(range(n))
+    
+    adj = defaultdict(list)
+    adjr = defaultdict(list)
+    for a, b in richer:
+      adj[b].append(a)
+      adjr[a].append(b)
+        
+    dq, d_resolved = deque(), [0]*n
+    for i in range(n):
+      d_resolved[i] = len(adj[i])
+      if d_resolved[i] == 0:
+        dq.append(i)
+          
+    while dq:
+      i = dq.popleft()
+      for nxt in adj[i]:
+        if quiet[res[nxt]] < quiet[res[i]]: res[i] = res[nxt]
+      for nxt in adjr[i]:
+        d_resolved[nxt] -= 1
+        if d_resolved[nxt] == 0: dq.append(nxt)
+    return res 
