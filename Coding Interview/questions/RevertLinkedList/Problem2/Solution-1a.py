da4fe0a635d53315e8f22e51784aa09e741834ea
JIAN LI <raymond.li.jian@gmail.com>
2024-11-17 07:39:32 -0800

    Create Solution-1a.py

---
+++ Coding Interview/questions/RevertLinkedList/Problem2/Solution-1a.py
@@ -0,0 +1,35 @@
+from linked_list import LinkedList
+from linked_list_node import LinkedListNode
+from linked_list_traversal import traverse_linked_list
+from linked_list_reversal import reverse_linked_list
+  
+def findMarkers(head, left, right):
+  left_prev, right_next = None, None 
+
+  prev, curr = head, head.next 
+  pos = 2
+  while curr:
+    if pos == left: left_prev = prev 
+    if pos == right: right_next = curr.next 
+    prev = curr 
+    curr = curr.next 
+    pos += 1
+  return left_prev, right_next
+    
+def reverse_between(head, left, right):
+  if head == None or head.next == None or left == right: return head 
+  
+  left_prev, right_next = findMarkers(head, left, right)
+  if left_prev == None: prev = head
+  else: prev = left_prev.next 
+  curr, new_tail = prev.next, right_next
+  while curr != right_next: 
+    # next = curr.next 
+    prev.next = new_tail 
+    new_tail = prev  
+    prev = curr 
+    curr = curr.next
+  prev.next = new_tail
+  if left_prev == None: return prev
+  left_prev.next = prev
+  return head
