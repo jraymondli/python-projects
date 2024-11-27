ac90da756830fec95849d3cd9ebfbae5da8d5ba6
JIAN LI <raymond.li.jian@gmail.com>
2024-11-27 09:53:01 -0800

    Create RaySolution.py

---
+++ Coding Interview/Algorithm/Greedy Algorithms/Examples/Jump Game/RaySolution.py
@@ -0,0 +1,16 @@
+def jump_game(nums):
+    
+    n = len(nums)
+    if n == 1: return True 
+    reached = [False] * n 
+    reached[0] = True 
+    
+    for i in range(n):
+      if not reached[i]: continue
+      for j in range(1, nums[i]+1):
+        nxt_index = i + j
+        if nxt_index >= n: continue 
+        if nxt_index == (n-1): return True 
+        reached[nxt_index] = True 
+
+    return False
